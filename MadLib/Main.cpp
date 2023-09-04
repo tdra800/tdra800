132b97e760cd820eb3c447e0451876e245b49a2e
tdra0804 <130621384+tdra800@users.noreply.github.com>
2023-09-04 11:54:57 -0500

    Basic functionality. Uses .txt file inputs

--- MadLib/Main.cpp
+++ MadLib/Main.cpp
@@ -6,7 +6,7 @@
 // Program flow:
 //   Read file and extract all necessary words. Store in a seperate string vector
 //   Ask user for all desired words
-//   Print MadLib with user words 
+//   Print MadLib with user words
 // 2nd option
 //   Read in MadLib into a vector, when a desired word is found stop.
 //   Ask user for word
@@ -14,15 +14,20 @@
 //   Read until end of MadLib
 //   Print full MadLib
 
-
 #include <iostream> // cout and cin
 #include <vector>
 #include <fstream> // opening files
+#include <string>
 
-void GetWord(std::string DesiredWord, std::string InputWord, std::vector<std::string> MadLibWords)
+// Pass the desired word type request (noun, verb, adj) and pass by ref MadLib class instance
+// Update the MadLibWords string vector with user input for the WordTypeReq
+void GetWord(std::string WordTypeReq, std::vector<std::string> &MadLibWords)
 {
+    // Def
+    std::string InputWord;
 
-    std::cout << "Enter a " << DesiredWord << " " << std::endl;
+    // Ask user for input
+    std::cout << "Enter a " << WordTypeReq << " " << std::endl;
     std::cin >> InputWord;
     MadLibWords.push_back(InputWord);
 }
@@ -30,29 +35,75 @@ void GetWord(std::string DesiredWord, std::string InputWord, std::vector<std::st
 int main()
 {
 
+    // Class for the MadLibWords that are read in from user input
     class MadLib
     {
     public:
         std::vector<std::string> MadLibWords;
-        std::vector<std::string> PresetWords{"Hello", "World", "Its", "Good", "To", "Be", "Here"};
-        std::string InputWord;
-
     };
 
     MadLib CurrMadLib;
 
+    // Open file with MadLib
+    std::ifstream MadLibFile;
+    MadLibFile.open("SchoolFoodMLSmall.txt");
 
-
-    // Add value for loop to run
-    for (int i = 0; i < 0; i++)
+    // Check file opened
+    if (!MadLibFile.is_open())
     {
-        std::cout << "Enter a word " << std::endl;
-        std::cin >> CurrMadLib.InputWord;
-
-        CurrMadLib.MadLibWords.push_back(CurrMadLib.InputWord);
+        std::cerr << "Error: Unable to open the file for reading. " << std::endl;
+        return 1; // return with error code
     }
 
-    std::cout << std::endl;
+    // Tried reading in char by char, check for () but the char array was full of garbage
+    // Can read in file word by word and store in class MadLib, vector string MadLibWords will
+    // hold the entire story
+    std::string SingleWord;
+    char c;
+    char FileWord[256];
+    int i = 0;
+    while (std::getline(MadLibFile, SingleWord, ' '))
+    {
+        std::size_t found = SingleWord.find("(");
+
+        // User input words are defined by ()
+        // Search the string line for a paraenthesis
+        // If found, it is a word requiring user input
+        // Remove paraentheses () and ask user for input
+        if (found != std::string::npos)
+        {
+            // Search through the string character by character and create new string without ( or )
+            std::string WordType = "";
+            for (char character : SingleWord)
+            {
+                if (character != '(' && character != ')')
+                {
+                    WordType += character;
+                }
+            }
+
+            // Ask user for input 
+            std::cout << "Please input a " << WordType << " ";
+            std::string UserInput;
+            std::cin >> UserInput;
+            UserInput += ' ';
+            CurrMadLib.MadLibWords.push_back(UserInput);
+
+        }
+        // No paraenthesis found, add to MadLib story
+        else {
+            SingleWord += ' ';
+            CurrMadLib.MadLibWords.push_back(SingleWord);
+        }
+        
+    }
+
+    // Print the story with the user input
+    for (int i = 0; i < CurrMadLib.MadLibWords.size(); i++)
+    {
+        std::cout << CurrMadLib.MadLibWords[i];
+    }
+
 
     /*
     for(int i = 0; i< PresetWords.size(); i++)
