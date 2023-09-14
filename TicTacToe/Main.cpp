37598cbaa1a0f4e064caf215c36e1061dc2f91ac
tdra0804 <130621384+tdra800@users.noreply.github.com>
2023-09-13 21:56:54 -0500

    prnt board, usr input, cpu rand choice, win chk

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "GameBoard.cpp"
+#include <random>
+#include "SimpleGameBoard.cpp"
 
 void PrintGrid(void)
 {
@@ -22,75 +23,33 @@ bool CheckForWin()
     return Win;
 }
 
-void PrintLine(char* ptr)
-{
-    for(int i = 0; i < 5; i++)
-    {
-        // Check if expected char |,X,O or an uint
-        if ( *ptr == '|' || *ptr == 'X' || *ptr == 'O') 
-        { 
-            std::cout << *ptr << " ";
-        }
-        else
-            std::cout << static_cast<int>(*ptr) << " ";
-        
-        ptr++;
-    }
-    std::cout << std::endl;
-}
-
 
 int main()
 {
+
+    TicTacToe Game;
+
+    // start game
     std::cout << "Hello, welcome to TicTacToe!" << std::endl << std::endl;
+    bool WinDet = false;
+    while (WinDet && !Game.BoardFull()) {
+        Game.GetPlyrInput();
+        Game.CheckPlyrInput();
+        Game.PlyrUpdBrds();
+        Game.CpuRanChoice();
+        Game.CpuUpdBrds();
+        std::cout << std::endl << "The CPU has choosen!" << std::endl;
+        std::cout << "Here is the board" << std::endl;
+        Game.PrintBoard(Game.MB);
+    }    
 
-    char BoardArray[15] = {1,'|',2,'|',3,4,'|',5,'|',6,7,'|',8,'|',9};
-    char* ptr = &BoardArray[0];
-    //print 1st line and divider line
-    PrintLine(ptr);
-    std::cout << "----------" << std::endl;
-    // Print 2nd line and divider 
-    PrintLine(ptr+5);
-    std::cout << "----------" << std::endl;
-    // Print 3nd line  
-    PrintLine(ptr+10);
-    std::cout << std::endl;
+    //char DisplayBoard[15] = {1, '|', 2, '|', 3, 4, '|', 5, '|', 6, 7, '|', 8, '|', 9};
+    char DisplayBoard[15] = {'O', '|', 'X', '|', 3, 'O', '|', 'X', '|', 6, 'O', '|', 'X', '|', 9};
 
-    // User input
-    int16_t UsrInput;
-    std::cout << "Please select a position for your X! " << std::endl;
-    std::cout << "Position: ";
-    std::cin >> UsrInput;
+   
 
-    // Is it valid?
-    while( !(UsrInput == 1 || UsrInput == 2 || UsrInput == 3 || UsrInput == 4 || UsrInput == 5 || UsrInput == 6 || UsrInput == 7 || UsrInput == 8) || UsrInput == 9)
-    {
-        std::cin.clear();   // Clear any error state
-        //std::cin.ignore(std::numeric_limits<std::streamsize>::max());   /// Ignore to the end of Stream 
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Ignore to the end of line
+    // Check columns for victory
 
-        std::cout << "Invalid entry, please only enter 1-9 and press enter ";
-        std::cin >> UsrInput;
-    }
-
-    // Update array
-    BoardArray[UsrInput -1] = 'X';
-
-    // ptr = &BoardArray[0];
-    // if (*ptr == 'X' || *ptr == 'O')
-    //     std::cout << *ptr << " | ";
-    // else 
-    //     std::cout << static_cast<int>(*ptr) << " | ";
-    
-    // std::cout << static_cast<int>(*ptr) << " | " << static_cast<int>(*(ptr+1)) << " | " << static_cast<int>(*(ptr+2)) << std::endl;
-    // std::cout << "---------" << std::endl;
-    // std::cout << static_cast<int>(*(ptr+3)) << " | " << static_cast<int>(*(ptr+4)) << " | " << static_cast<int>(*(ptr+5)) << std::endl;
-    // std::cout << "---------" << std::endl;
-    // std::cout << static_cast<int>(*(ptr+6)) << " | " << static_cast<int>(*(ptr+7)) << " | " << static_cast<int>(*(ptr+8)) << std::endl;
-
-    // Create gameboard object for game
-    GameBoard Game1;
-
-
+    std::cout << "?"; 
     return 0;
 }
--- TicTacToe/SimpleGameBoard.cpp
+++ TicTacToe/SimpleGameBoard.cpp
@@ -1,12 +1,207 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 
 // Class to manage marks on the board
-class GameBoard
+class TicTacToe
 {
-    public:
-        // 2x2 array to track marks
-        char Board[3][3];
+public:
+    void CheckPlyrInput();
+    void GetPlyrInput();
+    void PrintBoard(const char *ptr);
+    void PrintLine(const char *ptr);
+    void PlyrUpdBrds();
+    void CpuRanChoice();
+    void CpuUpdBrds();
+    bool CheckWinCond();
+    bool BoardFull();
+    const char *DB = &DisplayBoard[0];
+    const char *MB = &MarksBoard[0];
 
+private:
+    // 2x2 array to track marks
+    // Init variables
+    std::vector<int> AvailableNums = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // BoardArray prints the current board
+    char DisplayBoard[15] = {1, '|', 2, '|', 3, 4, '|', 5, '|', 6, 7, '|', 8, '|', 9};
+    char MarksBoard[15] = {' ', '|', ' ', '|', ' ', ' ', '|', ' ', '|', ' ', ' ', '|', ' ', '|', ' '}; // Start with empty board
+    int16_t PlyrInput;
+    int16_t CpuChoice;
 };
+
+void TicTacToe::PrintLine(const char *ptr)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        // Check if expected char |,X,O or an uint
+        if (*ptr == '|' || *ptr == 'X' || *ptr == 'O' || *ptr == ' ')
+        {
+            std::cout << *ptr << " ";
+        }
+        else
+            std::cout << static_cast<int>(*ptr) << " ";
+
+        ptr++;
+    }
+    std::cout << std::endl;
+}
+
+void TicTacToe::PrintBoard(const char *ptr)
+{
+    // print 1st line and divider line
+    PrintLine(ptr);
+    std::cout << "----------" << std::endl;
+    // Print 2nd line and divider
+    PrintLine(ptr + 5);
+    std::cout << "----------" << std::endl;
+    // Print 3nd line
+    PrintLine(ptr + 10);
+    std::cout << std::endl;
+}
+
+void TicTacToe::GetPlyrInput()
+{
+    std::cout << "Please select a listed position for your X! " << std::endl
+              << std::endl;
+    PrintBoard(DB);
+    std::cout << "Position: ";
+    std::cin >> PlyrInput;
+}
+
+void TicTacToe::CheckPlyrInput()
+{
+    // Check PlyrInput is valid
+    bool valid = false;                                                         // flag for valid input
+    auto it = std::find(AvailableNums.begin(), AvailableNums.end(), PlyrInput); // Search vector for PlyrInput, end of vector returned if val not found
+
+    if (it != AvailableNums.end())
+    {
+        valid = true;
+        AvailableNums.erase(it); // If val is found, delete
+    }
+
+    // If player input isn't valid, clear possible input stream error and ask user for another entry
+    // Check again
+    while (!valid)
+    {
+
+        std::cin.clear();                                                   // Clear any error state
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore to the end of line
+
+        // Print available numbers to player
+        std::cout << "Invalid entry, please enter ";
+        for (int i = 0; i < AvailableNums.size(); i++)
+            std::cout << AvailableNums[i] << ' ';
+        std::cout << std::endl;
+        std::cin >> PlyrInput;
+
+        auto it = std::find(AvailableNums.begin(), AvailableNums.end(), PlyrInput);
+        if (it != AvailableNums.end())
+        {
+            valid = true;
+            AvailableNums.erase(it);
+        }
+    }
+}
+
+void TicTacToe::PlyrUpdBrds()
+{
+    // Update display board to display not available space (empty)
+    // Update Maskboard with X as well
+    for (int i = 0; DisplayBoard[i] != '\0'; i++)
+    {
+        if (DisplayBoard[i] == PlyrInput)
+        {
+            DisplayBoard[i] = ' ';
+            MarksBoard[i] = 'X';
+            break;
+        }
+    }
+}
+
+void TicTacToe::CpuRanChoice()
+{
+    // Create a random number generator engine
+    std::random_device rd;                                   // Seed the generator with a random device
+    std::mt19937 gen(rd());                                  // Mersenne Twister engine, a commonly used random number generator
+    std::uniform_int_distribution<int> distribution(1, 100); // Define a random distribution (e.g., uniform distribution between 1 and 100)
+    bool valid2 = false;                                     // flag for valid input
+
+    // Create random choice between 1-9, check if available
+    // If not, create new choice
+    do
+    {
+        CpuChoice = distribution(gen) % 10;                                          // 9 is the number of squares for TicTacToe
+        auto it2 = std::find(AvailableNums.begin(), AvailableNums.end(), CpuChoice); // Search vector for CpuChoice, end of vector returned if val not found
+
+        if (it2 != AvailableNums.end())
+        {
+            valid2 = true;
+            AvailableNums.erase(it2);
+        }
+    } while (!valid2);
+}
+
+void TicTacToe::CpuUpdBrds()
+{
+    // Update display board to display not available space (empty)
+    // Update Maskboard with X as well
+    for (int i = 0; DisplayBoard[i] != '\0'; i++)
+    {
+        if (DisplayBoard[i] == CpuChoice)
+        {
+            DisplayBoard[i] = ' ';
+            MarksBoard[i] = 'O';
+            break;
+        }
+    }
+}
+
+bool TicTacToe::CheckWinCond()
+{
+    bool WinDet = false;
+    // Check rows for victory
+    //  Increments set to check row values in array
+    for (int StartPtr = 0; StartPtr < 11; StartPtr += 5)
+    {
+        if (DisplayBoard[StartPtr] == DisplayBoard[StartPtr + 2] && DisplayBoard[StartPtr + 2] == DisplayBoard[StartPtr + 4])
+        {
+            if (DisplayBoard[StartPtr] == 'X') // Check if player won
+            {
+                std::cout << "Victory!" << std::endl;
+                return true;
+            }
+            else if (DisplayBoard[StartPtr] == 'O') // Check if CPU won
+            {
+                std::cout << "Oh no! Defeat!" << std::endl;
+                return true;
+            }
+        }
+    }
+
+    // Check cols for victory
+    for (int StartPtr = 0; StartPtr < 11; StartPtr += 2)
+    {
+        if (DisplayBoard[StartPtr] == DisplayBoard[StartPtr + 5] && DisplayBoard[StartPtr + 5] == DisplayBoard[StartPtr + 10])
+        {
+            if (DisplayBoard[StartPtr] == 'X')
+            {
+                std::cout << "Victory!" << std::endl; // Check if player won
+                return true;
+            }
+            else if (DisplayBoard[StartPtr] == 'O') // Check if CPU won
+            {
+                std::cout << "Oh no! Defeat!" << std::endl;
+                return true;
+            }
+        }
+    }
+}
+
+bool TicTacToe::BoardFull()
+{
+    if (AvailableNums.empty())
+        return true;
+    else
+        return false;
+}
